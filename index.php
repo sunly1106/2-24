02cffcf31db6ea37a1dd6f75b59825c4c83b3583
sunly1106 <Aa0966547817@gmail.com>
2025-02-24 15:05:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>練習</title>
+</head>
+<body>
+    <h1>我的練習 php & mysql</h>
+    <p>今天是: <?= data("Y年m月d日")   ?>
+    </p>
+</body>
+</html>
