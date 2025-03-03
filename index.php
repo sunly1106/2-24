c5f1c435f29fa0f58dc58cfaa9a892ebd69061ce
sunly1106 <Aa0966547817@gmail.com>
2025-03-03 15:53:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,43 @@
 </head>
 <body>
     <h1>我的練習 php & mysql</h1>
-    <p>我的名字:<?=  $_GET["name"]   ?></p>
-    <p>我的年齡:<?=  $_GET["age"]   ?></p>
-    <p>今天是: <?=date("Y/m/d")?></p>
-    <p>我的身高是: <?= $h=$_GET["height"] ?></p>
-    <p>我的體重是: <?= $w=$_GET["weight"] ?></p>
-    <p>我的BMI 是:<?= $w/($h/100*$h/100) ?></p>
+
+    <form action="">
+    姓名:
+    <input type="text" name="name">
+    年齡:
+    <input type="text" name="age">
+    <br>
+    身高:
+    <input type="text" name="height">
+    體重:
+    <input type="text" name="weight">
+    <br>
+    <input type="submit" value"送出" name="submit" >
+    </form>
+    <hr>
+
+    <?php if ( isset($_GET["submit"])) { ?>
+        <p>我的名字:<?=  $_GET["name"]   ?></p>
+        <p>我的年齡:<?=  $_GET["age"]   ?></p>
+        <p>今天是: <?=date("Y/m/d")?></p>
+        <p>我的身高是: <?= $h=$_GET["height"] ?></p>
+        <p>我的體重是: <?= $w=$_GET["weight"] ?></p>
+        <p>我的BMI 是:<?=$bmi= $w/($h/100*$h/100) ?></p>
     
+    
+    
+    
+    <?php
+        if ($bmi>25){
+            echo "胖";
+        } elseif ($bmi<18){
+            echo "瘦";
+        } else {
+            echo "好好保持!";
+        }
+   ?> 
+    <?php } ?>
+
 </body>
 </html>
