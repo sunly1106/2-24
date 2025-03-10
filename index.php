d4daa470f5b704c9ab8769a9d112c9529754f368
sunly1106 <Aa0966547817@gmail.com>
2025-03-10 14:41:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,31 @@
 </head>
 <body>
     <h1>報名表</h1>
+ <form action=""> 
+
+    <fieldset>
+    <legend>基本資料</legend>
+    <P><label for="name">姓名 </label>
+        <input type="text" name="name" id="name" value="" placeholder="請用中文" required>
+    </P>
+    </fieldset>
+    <fieldset>
+        <legend>使用行為</legend>
+    </fieldset>
+    <fieldset>
+        <legend>滿意度</legend>
+    </fieldset>
+    <fieldset>
+        <legend>資料上傳</legend>
+    </fieldset>
+   
+
+
+
+
+
+
+
+    </form>  
 </body>
 </html>
