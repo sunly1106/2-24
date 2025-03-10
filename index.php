8d5ee73acb1eeb30e5694d7a56233ce8893c608d
sunly1106 <Aa0966547817@gmail.com>
2025-03-10 16:12:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,35 +49,51 @@
     </fieldset>
     <fieldset>
         <legend>使用行為</legend>
-        <input type="checkbox" name="behavior[]" id="behavior1">
-        <label for="behavior1">聊天</label>
-        <input type="checkbox" name="behavior[]" id="behavior2">
-        <label for="behavior2">直播</label>
-        <input type="checkbox" name="behavior[]" id="behavior3">
-        <label for="behavior3">書信</label>
-        <input type="checkbox" name="behavior[]" id="behavior4">
-        <label for="behavior4">社群</label>
-        <input type="checkbox" name="behavior[]" id="behavior5">
-        <label for="behavior5">購物</label>
-        <input type="checkbox" name="behavior[]" id="behavior6">
-        <label for="behavior6">金融</label>
+        <p>
+            <input type="checkbox" name="behavior[]" id="behavior1">
+            <label for="behavior1">聊天</label>
+            <input type="checkbox" name="behavior[]" id="behavior2">
+            <label for="behavior2">直播</label>
+            <input type="checkbox" name="behavior[]" id="behavior3">
+            <label for="behavior3">書信</label>
+            <input type="checkbox" name="behavior[]" id="behavior4">
+         <label for="behavior4">社群</label>
+            <input type="checkbox" name="behavior[]" id="behavior5">
+            <label for="behavior5">購物</label>
+            <input type="checkbox" name="behavior[]" id="behavior6">
+            <label for="behavior6">金融</label>
+        </p>
     </fieldset>
     <fieldset>
         <legend>滿意度</legend>
-        <label for="">場地</label>
-        <input type="radio" name="place" id="place1" value="5">
-        <label for="place1">非常滿意</label>
-        <input type="radio" name="place" id="place2" value="4">
-        <label for="place2">滿意</label>
-        <input type="radio" name="place" id="place3" value="3">
-        <label for="place3">普通</label>
-        <input type="radio" name="place" id="place4" value="2">
-        <label for="place4">不滿意</label>
-        <input type="radio" name="place" id="place5" value="1">
-        <label for="place5">非常不滿意</label>
+       <P>
+            <label for="">場地</label>
+            <input type="radio" name="place" id="place1" value="5">
+            <label for="place1">非常滿意</label>
+            <input type="radio" name="place" id="place2" value="4">
+            <label for="place2">滿意</label>
+            <input type="radio" name="place" id="place3" value="3">
+            <label for="place3">普通</label>
+            <input type="radio" name="place" id="place4" value="2">
+            <label for="place4">不滿意</label>
+            <input type="radio" name="place" id="place5" value="1">
+            <label for="place5">非常不滿意</label>
+        </P>
     </fieldset>
     <fieldset>
         <legend>資料上傳</legend>
+        <P>
+            <label for="">同意書</label>
+            <input type="file" name="agreement" id="agreement" accept=".doc,.doex">
+        </P>
+        <P>
+
+            <label for="">個人照片</label>
+            <input type="file" name="image" accept="image/*" onchange="preview_image(event)">
+
+        </P>
+
+
     </fieldset>
    
 
@@ -88,5 +104,15 @@
 
 
     </form>  
+    <script type='text/javascript'>
+function preview_image(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
+</script>
 </body>
 </html>
