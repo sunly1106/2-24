a9f18dbc072528e9773e034b5f09e4bacea24104
sunly1106 <Aa0966547817@gmail.com>
2025-03-10 14:04:28 +0800

    報名表

--- index.php
+++ index.php
@@ -3,47 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>練習</title>
+    <title>報名表</title>
 </head>
 <body>
-    <h1>我的練習 php & mysql</h1>
-
-    <form action="">
-    姓名:
-    <input type="text" name="name">
-    年齡:
-    <input type="text" name="age">
-    <br>
-    身高:
-    <input type="text" name="height">
-    體重:
-    <input type="text" name="weight">
-    <br>
-    <input type="submit" value"送出" name="submit" >
-    </form>
-    <hr>
-
-    <?php if ( isset($_GET["submit"])) { ?>
-        <p>我的名字:<?=  $_GET["name"]   ?></p>
-        <p>我的年齡:<?=  $_GET["age"]   ?></p>
-        <p>今天是: <?=date("Y/m/d")?></p>
-        <p>我的身高是: <?= $h=$_GET["height"] ?></p>
-        <p>我的體重是: <?= $w=$_GET["weight"] ?></p>
-        <p>我的BMI 是:<?=$bmi= $w/($h/100*$h/100) ?></p>
-    
-    
-    
-    
-    <?php
-        if ($bmi>25){
-            echo "胖";
-        } elseif ($bmi<18){
-            echo "瘦";
-        } else {
-            echo "好好保持!";
-        }
-   ?> 
-    <?php } ?>
-
+    <h1>報名表</h1>
 </body>
 </html>
